914845e7b47b90fd7c45c3aa22f098d10b77b87b
Bryan Burgess <bvburgess@protonmail.ch>
2025-05-21 16:41:45 -0400

    Wrangled data for updated data for 10/14 indicators to populate borderlands_datawrapper_v0.2.csv. Outstanding indicators are Currency Swaps, FDI, Special Economic Zones, and the United Front.

--- 01_centroids.R
+++ 01_centroids.R
@@ -16,32 +16,30 @@ setwd(data_dir)
 
 # Country Centroids -------------------------------------------------------
 
-
-
-centroids <- tribble(~country, ~lat, ~lon, 
+centroids <- tribble(~recipient, ~lat, ~lon, 
   "Afghanistan", 33.7680065, 66.2385139,
   "Bhutan", 27.5495110, 90.5119273,
-  "Brunei", 4.4137155, 114.5653908,
+  "Brunei Darussalam", 4.4137155, 114.5653908,
   "India", 22.3511148, 78.6677428,
   "Indonesia", -2.4833826, 117.8902853,
   "Japan", 36.5748441, 139.2394179,
   "Kazakhstan", 48.1012954, 66.7780818,
-  "Kyrgyzstan", 41.5089324, 74.7240910,
-  "Laos", 20.0171109, 103.3782530,
+  "Kyrgyz Republic", 41.5089324, 74.7240910,
+  "Lao People's Democratic Republic", 20.0171109, 103.3782530,
   "Malaysia", 4.5693754, 102.2656823,
   "Mongolia", 46.8250388, 103.8499736,
   "Myanmar", 17.1750495, 95.9999652,
   "Nepal", 28.3780464, 83.9999901,
-  "North Korea", 40.3736611, 127.0870417, 
+  "Democratic People's Republic of Korea", 40.3736611, 127.0870417, 
   "Pakistan", 30.3308401, 71.2474990,
   "Philippines", 14.5904492, 120.9803621,
   "Russia", 64.6863136, 97.7453061, 
   "South Korea", 36.6383920, 127.6961188,
   "Tajikistan", 38.6281733, 70.8156541,
-  "Vietnam", 15.9266657, 107.9650855
+  "Viet Nam", 15.9266657, 107.9650855
 )
 
 
 # Save centroids ----------------------------------------------------------
 write_rds(centroids, "borderlands_centroids.rds")
-
+write_csv(centroids, "borderlands_centroids.csv")
--- 02_nbr_wrangle.R
+++ 02_nbr_wrangle.R
@@ -1,7 +1,7 @@
 # NBR Borderlands Project
 
 # Created 2025-05-20
-# Updated 2025-05-20
+# Updated 2025-05-21
 # Author: Bryan Burgess
 
 # Reading and wrangling all the data. 
@@ -70,6 +70,11 @@ setwd("./ChinesePublicDiplomacyExport")
 pub_dip <- read_csv("ChinesePublicDiplomacy.csv") |>
   janitor::clean_names() |> 
   filter(receiving_country %in% country_list)
+# Fixing non-numeric columns
+pub_dip$year <- as.numeric(pub_dip$year)
+
+
+
 cis_ccs <- read_csv("Confucius-Institutes.csv") |>
   janitor::clean_names() |> 
   filter(country %in% country_list)
@@ -125,14 +130,13 @@ sec_01_arms_transfers <- sipri %>%
   filter(supplier == "China",
         year_of_order > 1999,
         recipient %in% country_list) %>%
-  group_by(recipient, supplier, year_of_order) %>%
+  group_by(recipient, year_of_order) %>%
   summarise(sec_01_arms_transfer_tiv = sum(sipri_tiv_for_total_order, na.rm = T),
             sec_01_arms_transfer_tiv_delivered = sum(sipri_tiv_of_delivered_weapons, na.rm = T),
             sec_01_arms_transfer_orders_ct = n()) %>%
   mutate(sec_01_missing_transfer_tiv = sec_01_arms_transfer_tiv - sec_01_arms_transfer_tiv_delivered) %>%
   select(year = year_of_order,
-         everything()) %>%
-  arrange(recipient, year)
+         everything())
 
 ## sec_02_surveillance -----------------------------------------------------
 
@@ -148,20 +152,54 @@ surveillance <- surveillance %>%
            sector_name == "TRADE POLICIES AND REGULATIONS") | sector_name == "TRANSPORT AND STORAGE") |
            aid_data_record_id == 96256) | aid_data_record_id == 63248)
 
-surveillance <- surveillance %>%
+sec_02_surveillance <- surveillance %>%
   group_by(commitment_year, recipient) %>%
   summarise(sec_02_surveillance_usd = sum(adjusted_amount_constant_usd_2024, na.rm = T),
-            sec_02_surveillance_ct = n())
+            sec_02_surveillance_ct = n()) %>%
+  select(year = commitment_year,
+         recipient, 
+         sec_02_surveillance_usd, 
+         sec_02_surveillance_ct)
 
-## sec_03_military_engagement -----------------------------------------------------
-## sec_04_joint_exercises -----------------------------------------------------
+## sec_03_military_engagement -------------------------------------------------
 
+sec_03_military_engagement <- mil_dip %>%
+  filter(year >= 1999,
+         activity_category == "Naval Port Call" |  activity_category == "Senior Level Visit") %>%
+  group_by(year, partner_country) %>%
+  summarise(sec_03_military_engagement_ct = n()) %>%
+  select(year, 
+         recipient = partner_country,
+         sec_03_military_engagement_ct)
 
+## sec_04_joint_exercise -----------------------------------------------------
+
+sec_04_joint_exercise <- mil_dip %>%
+  filter(year >= 1999,
+         activity_category == "Military Exercise") %>%
+  group_by(year, partner_country) %>%
+  summarise(sec_04_joint_exercise_ct = n()) %>%
+  select(year, 
+         recipient = partner_country,
+         sec_04_joint_exercise_ct)
 
 # Development Indicators --------------------------------------------------
 ## dev_01_currency_swap -----------------------------------------------------
+
 ## dev_02_infrastructure -----------------------------------------------------
+dev_02_infrastructure <- tuff %>%
+  filter(infrastructure == "Yes") %>%
+  group_by(commitment_year, recipient) %>%
+  summarise(dev_02_infrastructure_usd = sum(adjusted_amount_constant_usd_2024, na.rm = T),
+            dev_02_infrastructure_ct = n()) %>%
+  select(year = commitment_year,
+         recipient,
+         dev_02_infrastructure_usd,
+         dev_02_infrastructure_ct)
+
 ## dev_03_fdi -----------------------------------------------------
+
+
 ## dev_04_sez -----------------------------------------------------
 # NNeed to do some crative calculating of unique SEZs here. 
 # Confirm against IISS data: https://chinaconnects.iiss.org
@@ -169,28 +207,178 @@ surveillance <- surveillance %>%
 # Civilization Indicators -------------------------------------------------
 ## civ_01_united_front -----------------------------------------------------
 ## civ_02_healthcare -----------------------------------------------------
+civ_02_healthcare <- tuff %>%
+  filter(sector_name == "HEALTH") %>%
+  group_by(commitment_year, recipient) %>%
+  summarise(civ_02_healthcare_usd = sum(adjusted_amount_constant_usd_2024, na.rm = T),
+            civ_02_healthcare_ct = n()) %>%
+  select(year = commitment_year,
+         everything())
+
 ## civ_03_hlpv -----------------------------------------------------
+# outbound_political_visits The total number of visits by heads of state and heads of government from China to
+# target countries
+# inbound_political_visits The total number of visits by heads of state and heads of government from target
+# countries to China.
+pub_dip$outbound_political_visits <- as.numeric(pub_dip$outbound_political_visits)
+pub_dip$inbound_political_visits <- as.numeric(pub_dip$inbound_political_visits)
+
+civ_03_hlpv <- pub_dip %>%
+  filter(receiving_country %in% country_list,
+         !is.na(year)) %>%
+  select(year, 
+         recipient = receiving_country,
+         civ_03_outbound_visits_ct = outbound_political_visits,
+         civ_03_inbound_visits_ct = inbound_political_visits) %>%
+  mutate(civ_03_total_visits_ct = civ_03_outbound_visits_ct + civ_03_inbound_visits_ct)
+
 ## civ_04_csps -----------------------------------------------------
-## civ_05_judicial_engagement -----------------------------------------------------
+# Number of new content sharing partnerships established in a country in a given year.
+pub_dip$content_sharing_partnerships <- as.numeric(pub_dip$content_sharing_partnerships)
+
+civ_04_csps <- pub_dip %>%
+  filter(receiving_country %in% country_list,
+         !is.na(year)) %>%
+  select(year, 
+         recipient = receiving_country,
+         civ_04_csps_ct = content_sharing_partnerships)
+
+## civ_05_judicial_engagement --------------------------------------
+civ_05_judicial_engagement <- judicial %>%
+  group_by(year, bdl_country) %>%
+  summarise(civ_05_judicial_engagement_ct = n()) %>%
+  select(year,
+         recipient = bdl_country,
+         civ_05_judicial_engagement_ct) 
+  
+
 ## civ_06_ciccs -----------------------------------------------------
+# Because CIs and CC data have closures, a bit more complex, confirm across data.
+cis_open <- cis_ccs %>%
+  filter(country %in% country_list) %>%
+  group_by(country, year_opened) %>%
+  summarise(ci_open = n())
+cis_close <- cis_ccs %>%
+  filter(country %in% country_list) %>%
+  group_by(country, year_opened) %>%
+  summarise(ci_close = n())
+# Looks good, pub dip easier. 
+
+civ_06_ciccs <- pub_dip %>%
+  filter(receiving_country %in% country_list,
+         !is.na(year)) %>%
+  select(year, 
+         recipient = receiving_country,
+         civ_06_ci_ct = outbound_political_visits,
+         civ_06_cc_ct = inbound_political_visits) %>%
+  mutate(civ_06_ci_cc_ct = civ_06_ci_ct + civ_06_cc_ct)
 
 
+# Fix countries -----------------------------------------------------------
+# Standardize to TUFF
+table(tuff$recipient)
 
+table(sec_01_arms_transfers$recipient)
+sec_01_arms_transfers$recipient[sec_01_arms_transfers$recipient == "Laos"] <- "Lao People's Democratic Republic"
 
+# Tuff
+table(sec_02_surveillance$recipient)
 
+table(sec_03_military_engagement$recipient)
+sec_03_military_engagement$recipient[sec_03_military_engagement$recipient == "Brunei"] <- "Brunei Darussalam"
+sec_03_military_engagement$recipient[sec_03_military_engagement$recipient == "Kyrgyzstan"] <- "Kyrgyz Republic"
+sec_03_military_engagement$recipient[sec_03_military_engagement$recipient == "Laos"] <- "Lao People's Democratic Republic"
+sec_03_military_engagement$recipient[sec_03_military_engagement$recipient == "North Korea"] <- "Democratic People's Republic of Korea"
+sec_03_military_engagement$recipient[sec_03_military_engagement$recipient == "Vietnam"] <- "Viet Nam"
 
+table(sec_04_joint_exercise$recipient)
+sec_04_joint_exercise$recipient[sec_04_joint_exercise$recipient == "Brunei"] <- "Brunei Darussalam"
+sec_04_joint_exercise$recipient[sec_04_joint_exercise$recipient == "Kyrgyzstan"] <- "Kyrgyz Republic"
+sec_04_joint_exercise$recipient[sec_04_joint_exercise$recipient == "Laos"] <- "Lao People's Democratic Republic"
+sec_04_joint_exercise$recipient[sec_04_joint_exercise$recipient == "North Korea"] <- "Democratic People's Republic of Korea"
+sec_04_joint_exercise$recipient[sec_04_joint_exercise$recipient == "Vietnam"] <- "Viet Nam"
+# Tuff, it's fine
+table(dev_02_infrastructure$recipient)
+
+# Tuff, it's fine
+table(civ_02_healthcare$recipient)
+table(civ_03_hlpv$recipient)
+civ_03_hlpv$recipient[civ_03_hlpv$recipient == "Brunei"] <- "Brunei Darussalam"
+civ_03_hlpv$recipient[civ_03_hlpv$recipient == "Kyrgyzstan"] <- "Kyrgyz Republic"
+civ_03_hlpv$recipient[civ_03_hlpv$recipient == "Laos"] <- "Lao People's Democratic Republic"
+civ_03_hlpv$recipient[civ_03_hlpv$recipient == "North Korea"] <- "Democratic People's Republic of Korea"
+civ_03_hlpv$recipient[civ_03_hlpv$recipient == "Vietnam"] <- "Viet Nam"
+
+table(civ_04_csps$recipient)
+civ_04_csps$recipient[civ_04_csps$recipient == "Brunei"] <- "Brunei Darussalam"
+civ_04_csps$recipient[civ_04_csps$recipient == "Kyrgyzstan"] <- "Kyrgyz Republic"
+civ_04_csps$recipient[civ_04_csps$recipient == "Laos"] <- "Lao People's Democratic Republic"
+civ_04_csps$recipient[civ_04_csps$recipient == "North Korea"] <- "Democratic People's Republic of Korea"
+civ_04_csps$recipient[civ_04_csps$recipient == "Vietnam"] <- "Viet Nam"
+
+table(civ_05_judicial_engagement$recipient)
+civ_05_judicial_engagement$recipient[civ_05_judicial_engagement$recipient == "Brunei"] <- "Brunei Darussalam"
+civ_05_judicial_engagement$recipient[civ_05_judicial_engagement$recipient == "India "] <- "India"
+civ_05_judicial_engagement$recipient[civ_05_judicial_engagement$recipient == "Kyrgyzstan"] <- "Kyrgyz Republic"
+civ_05_judicial_engagement$recipient[civ_05_judicial_engagement$recipient == "Kyrgyzstan "] <- "Kyrgyz Republic"
+civ_05_judicial_engagement$recipient[civ_05_judicial_engagement$recipient == "Laos"] <- "Lao People's Democratic Republic"
+civ_05_judicial_engagement$recipient[civ_05_judicial_engagement$recipient == "Myanmar "] <- "Myanmar"
+civ_05_judicial_engagement$recipient[civ_05_judicial_engagement$recipient == "Pakistan "] <- "Pakistan"
+civ_05_judicial_engagement$recipient[civ_05_judicial_engagement$recipient == "Russia "] <- "Russia"
+civ_05_judicial_engagement$recipient[civ_05_judicial_engagement$recipient == "Taijikistan "] <- "Tajikistan"
+civ_05_judicial_engagement$recipient[civ_05_judicial_engagement$recipient == "Tajikistan "] <- "Tajikistan"
+civ_05_judicial_engagement$recipient[civ_05_judicial_engagement$recipient == "The Philippines"] <- "Philippines"
+civ_05_judicial_engagement$recipient[civ_05_judicial_engagement$recipient == "The Phillippines "] <- "Philippines"
+civ_05_judicial_engagement$recipient[civ_05_judicial_engagement$recipient == "The Phillipines "] <- "Philippines"
+civ_05_judicial_engagement$recipient[civ_05_judicial_engagement$recipient == "Uzbekistan "] <- "Uzbekistan"
+civ_05_judicial_engagement$recipient[civ_05_judicial_engagement$recipient == "Uzebekistan "] <- "Uzbekistan"
+civ_05_judicial_engagement$recipient[civ_05_judicial_engagement$recipient == "Vietnam"] <- "Viet Nam"
+civ_05_judicial_engagement$recipient[civ_05_judicial_engagement$recipient == "Vietnam "] <- "Viet Nam"
+
+civ_05_judicial_engagement <- civ_05_judicial_engagement %>%
+  filter(recipient != "Cambodia",
+         recipient != "Singapore",
+         recipient != "Thailand",
+         recipient != "Uzbekistan")
+
+table(civ_06_ciccs$recipient)
+civ_06_ciccs$recipient[civ_06_ciccs$recipient == "Brunei"] <- "Brunei Darussalam"
+civ_06_ciccs$recipient[civ_06_ciccs$recipient == "Kyrgyzstan"] <- "Kyrgyz Republic"
+civ_06_ciccs$recipient[civ_06_ciccs$recipient == "Laos"] <- "Lao People's Democratic Republic"
+civ_06_ciccs$recipient[civ_06_ciccs$recipient == "North Korea"] <- "Democratic People's Republic of Korea"
+civ_06_ciccs$recipient[civ_06_ciccs$recipient == "Vietnam"] <- "Viet Nam"
 
 # Join together -----------------------------------------------------------
 
+dash_data <- full_join(sec_01_arms_transfers, sec_02_surveillance, 
+                       by = c("year", "recipient")) %>%
+  full_join(sec_03_military_engagement, 
+            by = c("year", "recipient")) %>%
+  full_join(sec_04_joint_exercise, 
+            by = c("year", "recipient")) %>%
+  full_join(dev_02_infrastructure,
+            by = c("year", "recipient")) %>%
+  full_join(civ_02_healthcare, 
+            by = c("year", "recipient")) %>%
+  full_join(civ_03_hlpv,
+            by = c("year", "recipient")) %>%
+  full_join(civ_04_csps, 
+            by = c("year", "recipient")) %>%
+  full_join(civ_05_judicial_engagement,
+            by = c("year", "recipient")) %>%
+  full_join(civ_06_ciccs,
+            by = c("year", "recipient")) %>%
+  left_join(centroids, by = c("recipient"))
 
-dash_data <- left_join(centroids, pub_dip, by = c("country" = "receiving_country")) 
-
-
-
+table(dash_data$recipient)
 
 
 # Write data --------------------------------------------------------------
 setwd(datawrapper)
 
-# v0.1 2024-04-23 
+# v0.1 2025-04-23 
 # write_csv(dash_data, "borderlands_datawrapper_v0.1.csv")
+
+# v0.2 2025-05-21
+write_csv(dash_data, "borderlands_datawrapper_v0.2.csv")
+
